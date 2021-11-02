d2530ad46dec1808fe566ff95c44cbbde0c3e8a0
Tito <hdiaz@nectia.com>
2021-11-02 11:55:05 -0300

    create post endpoint

---
+++ library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -0,0 +1,19 @@
+package com.learnkafka.controller;
+
+import com.learnkafka.domain.LibraryEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibraryEventsController {
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+
+        //invoke kafka producer
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+}
